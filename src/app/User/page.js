bc75bb9f97ebc89b7ae9c6e06fb3cc2c20bf0962
Kavija <kavija.codezela@gmail.com>
2025-11-04 00:57:14 +0530

    Made the  back-end for Rendering post according to user's interested categories and fixed the bugs in few page.js (because we changed the api routes some of files had been trying to access those api's with old routes)

--- src/app/User/page.js
+++ src/app/User/page.js
@@ -5,10 +5,16 @@ import React, { useEffect, useState } from 'react'
 export default function page() {
 
   const [posts, setPosts] = useState([]);
+
   useEffect(() => {
 
     const fetchPosts = async () => {
-      const posts = await fetch ('../../api/user/post');
+
+      // first we need to get the userID from the cookies
+      const userIDData = await fetch('../api/token');
+      const userData = await userIDData.json();
+
+      const posts = await fetch (`../api/blog/post?uid=${userData.token}`);
 
       const data = await posts.json()
 
@@ -27,10 +33,10 @@ export default function page() {
         <div className='flex justify-center flex-wrap items-center w-full h-auto gap-2'>
         {posts.length > 0 ? (
           posts.map((post, index) => (
-            <div key={index} className='w-[300px] h-[150px] border border-black flex flex-col justify-center items-center'>
+            <Link key={index} className='w-[300px] h-[150px] border border-black flex flex-col justify-center items-center' href={`User/${post.PostID}`}>
               <p className='font-bold text-[20px] text-black'>{post.Topic}</p>
               <p className='text-[15px] text-gray-600'>{post.FName} {post.LName}</p>
-            </div>
+            </Link>
           ))
         ) : (
           <p>No posts available.</p>
--- src/app/loading.js
+++ src/app/loading.js
@@ -14,7 +14,7 @@ import Image from 'next/image'
 export default function loading() {
   return (
     <div className='flex w-full h-[100vh] justify-center items-center'>
-      <Image src="/loading.gif" alt='Loading Image' width={50} height={50}/>
+      LOADING...
     </div>
   )
 }
